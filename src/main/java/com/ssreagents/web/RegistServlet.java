727cd4772323351efe15750b784039b6e82685fc
Libra-JL <1332735161@qq.com>
2021-03-30 09:33:26 +0800

    路径修改

--- src/main/java/com/ssreagents/web/RegistServlet.java
+++ src/main/java/com/ssreagents/web/RegistServlet.java
@@ -19,21 +19,17 @@ public class RegistServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
-        String code = req.getParameter("code");
 
-        if ("abcde".equalsIgnoreCase(code)){
-            if(us.existsUsername(username)){
-                System.out.println("用户名【"+username+"】已存在");
-                req.getRequestDispatcher("/pages/user/regist.html").forward(req,resp);
-            }else {
-                us.registUser(new User(null,username,password,email));
-                req.getRequestDispatcher("/pages/user/regist_success.html");
-            }
-        }else {
-            System.out.println("验证码输入错误");
-            req.getRequestDispatcher("/pages/user/regist.html").forward(req,resp);
 
+        System.out.println("============================");
+        if (us.existsUsername(username)) {
+            System.out.println("用户名【" + username + "】已存在");
+            req.getRequestDispatcher("/ssreagents_war_exploded/pages/user/regist.html").forward(req, resp);
+        } else {
+            us.registUser(new User(null, username, password, email));
+            req.getRequestDispatcher("/ssreagents_war_exploded/pages/user/regist_success.html");
         }
+
 
     }
 }
